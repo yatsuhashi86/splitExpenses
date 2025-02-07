12667856a13b3940f6cf74bdedea0ced0c6a15e0
yatsuhashi86 <tomo38yatsuhashi@gmail.com>
2025-02-07 23:49:07 +0900

    debug画面作ってみたり入力ボタン作ってみたり ここからデータ入力してDB書き込んで。。っていう一番のとこ作りたい

--- app/src/main/java/com/example/splitexpenses/MainActivity.kt
+++ app/src/main/java/com/example/splitexpenses/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.splitexpenses
 
 import android.os.Bundle
 import android.view.View
+import android.widget.EditText
 import androidx.activity.ComponentActivity
 
 class MainActivity : ComponentActivity() {
@@ -12,7 +13,6 @@ class MainActivity : ComponentActivity() {
 
     fun continueButtonOnClick(view: View){
         setContentView(R.layout.home)
-
     }
 
     fun calenderButtonOnClick(view: View){
@@ -27,5 +27,9 @@ class MainActivity : ComponentActivity() {
         setContentView(R.layout.trading)
     }
 
+    fun enterPurchaseButtonOnClick(view: View){
+
+        setContentView(R.layout.debug)
+    }
 }
 
