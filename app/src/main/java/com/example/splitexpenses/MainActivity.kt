ec8825e8ef93627f91762f30b5d75acfe2fca2b0
yatsuhashi86 <tomo38yatsuhashi@gmail.com>
2025-02-08 13:38:50 +0900

    debug用のclass追加してみたけど、使い方はわかってない（）

--- app/src/main/java/com/example/splitexpenses/MainActivity.kt
+++ app/src/main/java/com/example/splitexpenses/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.example.splitexpenses
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.ComponentActivity
 
 class MainActivity : ComponentActivity() {
@@ -26,10 +29,17 @@ class MainActivity : ComponentActivity() {
     fun inputTradingButtonOnClick(view: View){
         setContentView(R.layout.trading)
     }
+    var debug = ""
+
 
     fun enterPurchaseButtonOnClick(view: View){
+        debug = findViewById(R.id.tradeMemo)
 
-        setContentView(R.layout.debug)
     }
+}
+
+class debugOnly(){
+    private var _binding: debugOnly? = null
+    private val binding get() = _binding!!
 }
 
