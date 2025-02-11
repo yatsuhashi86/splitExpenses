2ad14b4ffc4bfe8318b752fb09f9d4b00134fba7
yatsuhashi86 <tomo38yatsuhashi@gmail.com>
2025-02-11 22:04:37 +0900

    binding迷走中

--- app/src/main/java/com/example/splitexpenses/MainActivity.kt
+++ app/src/main/java/com/example/splitexpenses/MainActivity.kt
@@ -1,17 +1,37 @@
 package com.example.splitexpenses
 
 import android.annotation.SuppressLint
+import android.app.Fragment
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import androidx.activity.ComponentActivity
+import androidx.compose.ui.layout.PinnableContainer
+import com.example.splitexpenses.databinding.CalenderBinding
+import com.example.splitexpenses.databinding.HomeBinding
+import com.example.splitexpenses.databinding.LoginBinding
+import com.example.splitexpenses.databinding.RatioBinding
+import com.example.splitexpenses.databinding.TradingBinding
 
 class MainActivity : ComponentActivity() {
+    private lateinit var loginBinding: LoginBinding
+    private lateinit var homeBinding: HomeBinding
+    private lateinit var tradingBinding: TradingBinding
+    private lateinit var calenderBinding: CalenderBinding
+    private lateinit var ratioBinding: RatioBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        loginBinding = LoginBinding.inflate(layoutInflater)
+        homeBinding = HomeBinding.inflate(layoutInflater)
+        tradingBinding = TradingBinding.inflate(layoutInflater)
+        calenderBinding = CalenderBinding.inflate(layoutInflater)
+        ratioBinding = RatioBinding.inflate(layoutInflater)
         setContentView(R.layout.login)
+
     }
 
     fun continueButtonOnClick(view: View){
@@ -28,18 +48,47 @@ class MainActivity : ComponentActivity() {
 
     fun inputTradingButtonOnClick(view: View){
         setContentView(R.layout.trading)
+
     }
     var debug = ""
 
+    var _binding: TradingBinding = null
+    private val binding get() = _binding!!
+    fun onCreateView(
+        inflater: LayoutInflater?,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        //return inflater.inflate(inflater, container, false)
+        _binding = debugOnly.inflate(inflater, container, false)
+        return binding.root
+    }
 
-    fun enterPurchaseButtonOnClick(view: View){
-        debug = findViewById(R.id.tradeMemo)
+    fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 
+}
+
+class debugOnly: Fragment() {
+    var _binding: debugOnly? = null
+    private val binding get() = _binding!!
+    override fun onCreateView(
+        inflater: LayoutInflater?,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        //return inflater.inflate(inflater, container, false)
+        _binding = debugOnly.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
 
-class debugOnly(){
-    private var _binding: debugOnly? = null
-    private val binding get() = _binding!!
-}
+
 
